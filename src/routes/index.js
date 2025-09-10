aebb88b0228ff6ecfbc490fbb549c8d48042e0d2
Kenn0679 <kenn0679@gmail.com>
2025-09-10 21:58:26 +0700

    feat: remove health check route and related imports

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,5 @@
 import { Router } from 'express';
-import healthRouter from './modules/health.route.js';
 
 const router = Router();
-
-router.use('/health', healthRouter);
 
 export default router;
