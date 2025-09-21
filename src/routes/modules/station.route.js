34addcfcf9c0f788fe9a370a86e79d474340c55a
Kenn0679 <kenn0679@gmail.com>
2025-09-21 18:26:43 +0700

    feat: update zod version and implement staff management routes and controller with authentication and authorization

--- src/routes/modules/station.route.js
+++ src/routes/modules/station.route.js
@@ -8,22 +8,34 @@ import {
   softDeleteStation,
   deleteStaion,
 } from '../controllers/station.controller.js';
+import { authenticate } from '../../middleware/authenticate.js';
+import { authorize } from '../../middleware/authorize.js';
 
 const router = Router();
 
 // Lấy tất cả station (active)
-router.get('/', getStations);
+router.get('/', authenticate, authorize('ADMIN', 'STAFF'), getStations);
 // Lấy station theo id
-router.get('/:id', getStationByID);
+router.get('/:id', authenticate, authorize('ADMIN', 'STAFF'), getStationByID);
 // Lấy các station đã xóa hoặc inactive
-router.get('/unavailable/all', getUnavailableStations);
+router.get(
+  '/unavailable/all',
+  authenticate,
+  authorize('ADMIN', 'STAFF'),
+  getUnavailableStations
+);
 // Tạo station mới
-router.post('/', createStation);
+router.post('/', authenticate, authorize('ADMIN', 'STAFF'), createStation);
 // Cập nhật station
-router.put('/:id', updateStation);
+router.put('/:id', authenticate, authorize('ADMIN', 'STAFF'), updateStation);
 // Xóa mềm station (chuyển softDeleted=true)
-router.patch('/soft-delete/:id', softDeleteStation);
+router.patch(
+  '/soft-delete/:id',
+  authenticate,
+  authorize('ADMIN', 'STAFF'),
+  softDeleteStation
+);
 // Xóa cứng station (xóa khỏi DB)
-router.delete('/:id', deleteStaion);
+router.delete('/:id', authenticate, authorize('ADMIN'), deleteStaion);
 
 export default router;
